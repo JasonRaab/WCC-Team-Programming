5437e22d7b408f54e74aa8f26c950f81c4280067
TopherBriggs <christophertbriggs@gmail.com>
2022-03-21 20:14:03 -0400

    Created users class

---
+++ Management_Website/src/main/java/edu/wccnet/ctbriggs/springMVC/domain/Users.java
@@ -0,0 +1,64 @@
+package edu.wccnet.ctbriggs.springMVC.domain;
+
+
+public class Users {
+	private int userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String role;
+	private int isActive;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public int getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(int isActive) {
+		this.isActive = isActive;
+	}
+	public Users(String firstName, String lastName, String email, String password, String role, int isActive) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+		this.isActive = isActive;
+	}
+	public Users() {}
+}
